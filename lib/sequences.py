bd1dab7725438d0578683fd16c74e817d0d7e466
Thecla Owano <teckowano@gmail.com>
2025-11-13 14:35:45 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    """Print the first n Fibonacci numbers as a list: [0, 1, 1, …]"""
+    if n <= 0:
+        print("[]")
+        return
+
+    fib = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib.append(a)
+        a, b = b, a + b
+
+    # Build the required string: "[0, 1, 1, ...]"
+    print("[" + ", ".join(map(str, fib)) + "]")
